0ab5c01fb47057598506cf9a803c06e0128880de
ridhomujizat <ridhomujizat@gmail.com>
2021-04-19 00:25:29 +0700

    Create Hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+
 import Header from 'parts/Header'
+import Hero from 'parts/Hero'
+
+import landingpage from 'json/landingPage.json'
+
 
 export default class LandingPage extends Component {
   render () {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingpage.hero} />
       </>
     )
   }
