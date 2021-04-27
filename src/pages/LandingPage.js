8debec4416398d9e0db0ed0114f2f14cfcb03a9b
ridhomujizat <ridhomujizat@gmail.com>
2021-04-28 05:54:28 +0700

    create section most picked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react'
 
 import Header from 'parts/Header'
 import Hero from 'parts/Hero'
+import MostPicked from 'parts/MostPicked'
 
 import landingpage from 'json/landingPage.json'
 
-
 export default class LandingPage extends Component {
+  constructor(props){
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render () {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingpage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingpage.hero} />
+        <MostPicked refMostPicked={this.refMostPicked} data={landingpage.mostPicked} />
       </>
     )
   }
